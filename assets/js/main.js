9bdc308b619b781108439e720693ce3799e6a24e
Maria Eduarda Araujo <maeduardaaraujo01@gmail.com>
2023-04-07 16:14:20 -0300

    commit 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,57 @@
-const offset = 0;
-const limit = 40;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+const typeTranslations = {
+  normal: "Normal",
+  fighting: "Lutador",
+  flying: "Voador",
+  poison: "Veneno",
+  ground: "Terra",
+  rock: "Pedra",
+  bug: "Inseto",
+  ghost: "Fantasma",
+  steel: "Aço",
+  fire: "Fogo",
+  water: "Água",
+  grass: "Grama",
+  electric: "Elétrico",
+  psychic: "Psíquico",
+  ice: "Gelo",
+  dragon: "Dragão",
+  dark: "Sombrio",
+  fairy: "Fada",
+};
 
 function convertPokemonToLi(pokemon, index) {
   const number = index + 1;
-  return `<li class="pokemon">
-  <span class="numero">#${number.toString().padStart(3, "0")}</span>
-  <span class="nome">${pokemon.name}</span>
+  const pokemonId = pokemon.url.split("/").slice(-2, -1)[0];
+  const pokemonUrl = `https://unpkg.com/css-chain-test@1.0.8/src/PokeApi-Explorer.html?id=${pokemonId}`;
 
-  <div class="detalhes">
-    <ol class="botao">
-      <li class="tipo">Saiba Mais</li>
+  // Faz uma nova requisição para obter informações detalhadas sobre o Pokémon
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then((data) => {
+      const { types, species } = data;
+      const typeNames = types
+        .map((type) => typeTranslations[type.type.name])
+        .join(", ");
+      const speciesName = species.name;
 
-    </ol>
+      return `<li class="pokemon">
+        <span class="numero">#${number.toString().padStart(3, "0")}</span>
+        <span class="nome">${pokemon.name}</span>
+        <span class="tipo">${typeNames}</span>
 
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${number}.gif" alt=${
-    pokemon.name
-  }>
+        <div class="detalhes">
+          <ol class="botao">
+            <li class="tipo"><a href="${pokemonUrl}" target="_blank">Saiba Mais</a></li>
+          </ol>
 
-  </div>
-</li>
-`;
+          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${number}.gif" alt="${
+        pokemon.name
+      }">
+        </div>
+      </li>
+      `;
+    })
+    .catch((error) => console.error(error));
 }
 
 const pokemonLista = document.getElementById(`pokemonLista`);
@@ -30,7 +61,9 @@ pokeApi
   .then((pokemons) => {
     for (let i = 0; i < pokemons.length; i++) {
       const pokemon = pokemons[i];
-      pokemonLista.innerHTML += convertPokemonToLi(pokemon, i);
+      convertPokemonToLi(pokemon, i).then((html) => {
+        pokemonLista.innerHTML += html;
+      });
     }
   })
   .catch((error) => {
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,6 +1,6 @@
 const pokeApi = {};
 
-pokeApi.getPokemons = (offset = 0, limit = 40) => {
+pokeApi.getPokemons = (offset = 0, limit = 74) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
   return fetch(url)
     .then((response) => {
